897eda716ef6522c6e5e7dc2a4e8b987d9dd65c8
Todd Peterson <ToddPetersonGV@gmail.com>
2020-08-08 07:57:30 -0600

    remove bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="app">
         <router-view></router-view>
-        <div class="alert alert-primary" role="alert">Bootstrap is working</div>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// Bootstrap
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
 Vue.config.productionTip = false
 
 new Vue({
