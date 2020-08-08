82740c3afcda1066a420583d0e1d6ad5fc5523d5
Todd Peterson <ToddPetersonGV@gmail.com>
2020-08-08 08:14:33 -0600

    Set up basic routing

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="content">
+            <div class="logo">
+                <router-link :to="{name: 'home'}">Stock Trader</router-link>
+            </div>
+            <ul class="nav-links">
+                <router-link tag="li" :to="{name: 'stocks'}">Stocks</router-link>
+                <router-link tag="li" :to="{name: 'portfolio'}">Portfolio</router-link>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,27 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from '../views/Home.vue';
+import Stocks from '../views/Stocks.vue'
+import Portfolio from '../views/Portfolio.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: '/',
-        name: 'Home',
+        name: 'home',
         component: Home,
     },
+    {
+        path: '/stocks',
+        name: 'stocks',
+        component: Stocks
+    },
+    {
+        path: '/portfolio',
+        name: 'portfolio',
+        component: Portfolio
+    }
 ];
 
 const router = new VueRouter({
