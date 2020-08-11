5f86373ac9a38b29840e8e1596ddc527047b9f5d
Todd Peterson <ToddPetersonGV@gmail.com>
2020-08-11 11:00:48 -0600

    Move StockTradeList component to Trade page

--- src/views/Trade.vue
+++ src/views/Trade.vue
@@ -1,22 +1,34 @@
 <template>
     <div class="container">
         <div class="content">
-            <stock-trade-list></stock-trade-list>
+            <h3 class="overline section-title">Stocks</h3>
+            <div class="list-container">
+                <stock-trade v-for="company in companies" :key="company.id" :company="company"></stock-trade>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import StockTradeList from '../components/StockTradeList.vue'
+import { mapState } from 'vuex';
+import StockTrade from '../components/StockTrade.vue';
 import StatBar from '../components/StatBar.vue'
 
 export default {
+    computed: mapState(['companies']),
     components: {
-        StockTradeList,
-        StatBar
+        StockTrade,
+        StatBar,
     }
 }
 </script>
 
 <style scoped>
+.list-container {
+    margin-top: 1.8rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: stretch;
+}
 </style>
